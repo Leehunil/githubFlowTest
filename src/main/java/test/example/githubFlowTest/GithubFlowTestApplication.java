e77b80faf45d526ac73dc00b950a2a163782eb5a
ihun-il <hunil9978@gmail.com>
2024-01-09 14:07:10 +0900

    초기 세팅

---
+++ src/main/java/test/example/githubFlowTest/GithubFlowTestApplication.java
@@ -0,0 +1,13 @@
+package test.example.githubFlowTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubFlowTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubFlowTestApplication.class, args);
+	}
+
+}
